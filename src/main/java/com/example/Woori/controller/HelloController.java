4ae2459bf0d441aa96ff6182492188bb45829886
30308-78\3030878 <bys4034@gmail.com>
2024-07-30 16:35:29 +0900

    Initial commit

---
+++ src/main/java/com/example/Woori/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.Woori.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/test")
+    public String hello() {
+        return "테스트.";
+    }
+
+
+}
---
+++ src/test/java/com/example/Woori/WooriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Woori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
